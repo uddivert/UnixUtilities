65a910d1bb5268b6901b416b509f5b3e058c8baa
uddivert <uddhavswami@gmail.com>
2021-11-11 21:57:24 -0500

    added gitignore and shell code

---
+++ wc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// needs  -clw [ files
+
+int lines() { return 0;}
+int words () { return 0;}
+int bytes() { return 0;}
+
+int main (int argc, char *argv[]) {
+    printf("Hello World");
+    return 0;
+} // main
